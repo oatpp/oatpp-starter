bbab40c660fe726b1b00197e20b8982fc5ffc618
lganzzzo <lganzzzo@gmail.com>
2018-04-26 02:21:24 +0300

    SimpleAPI starter project created

---
+++ src/controller/MyController.hpp
@@ -0,0 +1,62 @@
+//
+//  MyController.hpp
+//  web-starter-project
+//
+//  Created by Leonid on 2/12/18.
+//  Copyright © 2018 oatpp. All rights reserved.
+//
+
+#ifndef MyController_hpp
+#define MyController_hpp
+
+#include "dto/MyDto.hpp"
+
+#include "oatpp/web/server/HttpError.hpp"
+#include "oatpp/web/server/api/ApiController.hpp"
+#include "oatpp/parser/json/mapping/ObjectMapper.hpp"
+#include "oatpp/core/macro/codegen.hpp"
+#include "oatpp/core/macro/component.hpp"
+
+/**
+ *  EXAMPLE ApiController
+ *  Basic examples of howto create ENDPOINTs
+ *  More details on oatpp.io
+ */
+class MyController : public oatpp::web::server::api::ApiController {
+protected:
+  MyController(const std::shared_ptr<ObjectMapper>& objectMapper)
+    : oatpp::web::server::api::ApiController(objectMapper)
+  {}
+public:
+  
+  /**
+   *  Inject @objectMapper component here as default parameter
+   *  Do not return bare Controllable* object! use shared_ptr!
+   */
+  static std::shared_ptr<MyController> createShared(OATPP_COMPONENT(std::shared_ptr<ObjectMapper>,
+                                                                 objectMapper)){
+    return std::shared_ptr<MyController>(new MyController(objectMapper));
+  }
+  
+  /**
+   *  Begin ENDPOINTs generation ('ApiController' codegen)
+   */
+#include OATPP_CODEGEN_BEGIN(ApiController)
+  
+  ENDPOINT("GET", "/", root) {
+    auto dto = MyDto::createShared();
+    dto->statusCode = 200;
+    dto->message = "Hello World!";
+    return createDtoResponse(Status::CODE_200, dto);
+  }
+  
+  // TODO Insert Your endpoints here !!!
+  
+  /**
+   *  Finish ENDPOINTs generation ('ApiController' codegen)
+   */
+#include OATPP_CODEGEN_END(ApiController)
+  
+};
+
+#endif /* MyController_hpp */

cefadd6df2a6f581d1a505a8a9c5eebdbd6e62a9
lganzzzo <lganzzzo@gmail.com>
2019-02-12 11:21:58 +0200

    clean up unnecessary code

--- main/src/App.cpp
+++ main/src/App.cpp
@@ -6,32 +6,11 @@
 //  Copyright © 2018 oatpp. All rights reserved.
 //
 
-//#define OATPP_USE_TARGET
-//#define OATPP_TARGET_TEST
-
-//////////////////////////////////
-// App
-
 #include "./controller/MyController.hpp"
 #include "./AppComponent.hpp"
 #include "./Logger.hpp"
 
-//////////////////////////////////
-// Test
-
-#ifdef OATPP_TARGET_TEST
-  #include "test/ControllerLevelTest.hpp"
-  #include "test/RouterLevelTest.hpp"
-  #include "test/ConnectionHandlerLevelTest.hpp"
-#endif
-
-//////////////////////////////////
-// oatpp
-
 #include "oatpp/network/server/Server.hpp"
-
-//////////////////////////////////
-// std
 
 #include <iostream>
 
@@ -49,8 +28,8 @@ void run() {
   
   auto router = components.httpRouter.getObject();
   
-  auto MyController = MyController::createShared();
-  MyController->addEndpointsToRouter(router);
+  auto myController = MyController::createShared();
+  myController->addEndpointsToRouter(router);
   
   /* create server */
   
@@ -70,16 +49,8 @@ int main(int argc, const char * argv[]) {
   
   oatpp::base::Environment::setLogger(new Logger());
   oatpp::base::Environment::init();
-  
-#if !defined(OATPP_USE_TARGET) | defined(OATPP_TARGET_APP)
+
   run();
-#endif
-  
-#ifdef OATPP_TARGET_TEST
-  OATPP_RUN_TEST(ControllerLevelTest);
-  OATPP_RUN_TEST(RouterLevelTest);
-  OATPP_RUN_TEST(ControllerLevelTest);
-#endif
   
   oatpp::base::Environment::setLogger(nullptr); ///< free Logger
   

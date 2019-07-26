04071de16924ceb50e027f2c1e43a5d8f255bc72
lganzzzo <lganzzzo@gmail.com>
2019-07-26 04:38:27 +0400

    updated to the latest oatpp API version

--- test/MyControllerTest.cpp
+++ test/MyControllerTest.cpp
@@ -47,7 +47,7 @@ void MyControllerTest::onRun() {
     OATPP_ASSERT(response->getStatusCode() == 200);
 
     /* Read response body as MessageDto */
-    auto message = response->readBodyToDto<MessageDto>(objectMapper);
+    auto message = response->readBodyToDto<MessageDto>(objectMapper.get());
 
     /* Assert that received message is as expected */
     OATPP_ASSERT(message);

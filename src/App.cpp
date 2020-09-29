eac2c0be791d42bb827c4fc452a1c493fab4878d
lganzzzo <lganzzzo@gmail.com>
2020-09-29 04:48:52 +0300

    Upgrade to the latest oatpp API.

--- src/App.cpp
+++ src/App.cpp
@@ -1,7 +1,7 @@
 #include "./controller/MyController.hpp"
 #include "./AppComponent.hpp"
 
-#include "oatpp/network/server/Server.hpp"
+#include "oatpp/network/Server.hpp"
 
 #include <iostream>
 
@@ -18,13 +18,13 @@ void run() {
   myController->addEndpointsToRouter(router);
 
   /* Get connection handler component */
-  OATPP_COMPONENT(std::shared_ptr<oatpp::network::server::ConnectionHandler>, connectionHandler);
+  OATPP_COMPONENT(std::shared_ptr<oatpp::network::ConnectionHandler>, connectionHandler);
 
   /* Get connection provider component */
   OATPP_COMPONENT(std::shared_ptr<oatpp::network::ServerConnectionProvider>, connectionProvider);
 
   /* Create server which takes provided TCP connections and passes them to HTTP connection handler */
-  oatpp::network::server::Server server(connectionProvider, connectionHandler);
+  oatpp::network::Server server(connectionProvider, connectionHandler);
 
   /* Priny info about server port */
   OATPP_LOGI("MyApp", "Server running on port %s", connectionProvider->getProperty("port").getData());
--- src/AppComponent.hpp
+++ src/AppComponent.hpp
@@ -3,7 +3,7 @@
 
 #include "oatpp/web/server/HttpConnectionHandler.hpp"
 
-#include "oatpp/network/server/SimpleTCPConnectionProvider.hpp"
+#include "oatpp/network/tcp/server/ConnectionProvider.hpp"
 
 #include "oatpp/parser/json/mapping/ObjectMapper.hpp"
 
@@ -20,7 +20,7 @@ public:
    *  Create ConnectionProvider component which listens on the port
    */
   OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::network::ServerConnectionProvider>, serverConnectionProvider)([] {
-    return oatpp::network::server::SimpleTCPConnectionProvider::createShared(8000);
+    return oatpp::network::tcp::server::ConnectionProvider::createShared({"localhost", 8000});
   }());
   
   /**
@@ -33,7 +33,7 @@ public:
   /**
    *  Create ConnectionHandler component which uses Router component to route requests
    */
-  OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::network::server::ConnectionHandler>, serverConnectionHandler)([] {
+  OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::network::ConnectionHandler>, serverConnectionHandler)([] {
     OATPP_COMPONENT(std::shared_ptr<oatpp::web::server::HttpRouter>, router); // get Router component
     return oatpp::web::server::HttpConnectionHandler::createShared(router);
   }());
